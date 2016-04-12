79f0fd13d78955b6a39f321ff34ccdbd39a46569
Arun Kutty <kutty.a@gmail.com>
2016-04-12 21:36:49 +0530

    Baselining demo version of expenseVouchers API

--- common/models/voucher.js
+++ common/models/voucher.js
@@ -1,3 +1,20 @@
 module.exports = function(Voucher) {
+  /*function getFinancialYrFromDate(date){
+    *//*
+    If Jan, Feb, Mar - "date.year-1" + "-" + "date.year"
+    Else - "date.year" + "-" + "date.year+1"
+     *//*
+    return 1;
 
+  }
+  Voucher.observe('before save', function generateVoucherNumber(ctx, next){
+    if (ctx.instance) {
+      //Vr No already generated
+      next();
+    } else {
+      var FY = getFinancialYrFromDate(ctx.data.Date);
+      ctx.data.VrNo = 123;
+    }
+    next();
+  });*/
 };
